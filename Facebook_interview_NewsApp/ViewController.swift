5e843f6e84d0a014bd7fc83718e5ebe53060da9c
Jian Ma <jianquanma@gmail.com>
2021-01-07 16:39:44 -0500

    added everything but the constants

---
+++ Facebook_interview_NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Facebook_interview_NewsApp
+//
+//  Created by Jian Ma on 1/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+// added something
+    // added sth
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Facebook_interview_NewsAppTests/Facebook_interview_NewsAppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Facebook_interview_NewsAppTests.swift
+//  Facebook_interview_NewsAppTests
+//
+//  Created by Jian Ma on 1/7/21.
+//
+
+import XCTest
+@testable import Facebook_interview_NewsApp
+
+class Facebook_interview_NewsAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
